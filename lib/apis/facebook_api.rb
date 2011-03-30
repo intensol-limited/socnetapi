e9546aec137bc7786dc2ee4d1e0c0cc0278ab56b
Dorialan <dorialan@gmail.com>
2011-03-30 17:59:03 +0300

    Facebook. Данные на выходе

--- lib/apis/facebook_api.rb
+++ lib/apis/facebook_api.rb
@@ -10,15 +10,15 @@ module Socnetapi
     end
     
     def friends
-      @facebook.get_connections("me", "friends")
+      prepare_friends(@facebook.get_connections("me", "friends")) rescue []
     end
     
     def entries
-      @facebook.get_connections("me", "home")
+      prepare_entries(@facebook.get_connections("me", "home")) rescue []
     end
     
     def entry id
-      @facebook.get_object(id) || nil
+      prepare_entry(@facebook.get_object(id))
     end
     
     def create params = {}
@@ -34,6 +34,59 @@ module Socnetapi
       @facebook.delete_object(id)
       res = @facebook.put_wall_post(params[:body])
       res["id"]
+    end
+    
+    private
+    
+    # id: 1056227917_193040020731897
+    # from: 
+    #   name: Yevgeniy Ikhelzon
+    #   id: "1056227917"
+    # message: "..."
+    # picture: http://external.ak.fbcdn.net/safe_image.php?d=4487dc4100c768685974c736080fd1a8&w=90&h=90&url=http%3A%2F%2Fs.kyivpost.ua%2Fimages%2Fstory_thumb%2Fdata%2Fuploads%2Fc%2Fiblock%2Fru_articles%2F89953%2F4030.jpg
+    # link: http://www.kyivpost.ua/ukraine/news/kolomojskij-ni-odna-gazeta-po-kievski-ne-vyjdet-poka-ne-ujdet-glavred.html
+    # name: "..."
+    # caption: www.kyivpost.ua
+    # description: "..."
+    # icon: http://static.ak.fbcdn.net/rsrc.php/v1/yD/r/aS8ecmYRys0.gif
+    # actions: 
+    # - name: Comment
+    #   link: http://www.facebook.com/1056227917/posts/193040020731897
+    # - name: Like
+    #   link: http://www.facebook.com/1056227917/posts/193040020731897
+    # type: link
+    # created_time: 2011-03-30T13:36:55+0000
+    # updated_time: 2011-03-30T13:36:55+0000
+    def prepare_entry entry
+      return unless entry
+      {
+        id: entry["id"],
+        author: {
+          id: entry["from"]["id"],
+          name: entry["from"]["name"]
+        },
+        title: entry["name"],
+        text: entry["message"],
+        attachments: {
+          images: entry["picture"] ? [entry["picture"]] : []
+        },
+        created_at: entry["created_at"]
+      }
+    end
+    
+    def prepare_entries entries
+      entries.map do |entry|
+        prepare_entry entry
+      end
+    end
+    
+    def prepare_friends friends
+      friends.map do |friend|
+        {
+          id: friend["id"],
+          name: friend["name"]
+        }
+      end
     end
   end
 end
