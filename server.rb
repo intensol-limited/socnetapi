14ea9afa2ba6bcdb0eeff508f0be95423e077eb3
Artem Pechenyi <pechkinator@gmail.com>
2011-06-13 12:03:47 +0300

    friendster login routine

--- server.rb
+++ server.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'oauth'
+require 'cgi'
+require 'nokogiri'
 
 config = YAML::load_file 'spec/config.yml'
 
@@ -48,4 +50,31 @@ get '/youtube_callback' do
   
   session[:access_token] = @access_token
   "Token: #{@access_token.token}<br />Secret: #{@access_token.secret}"
+end
+
+get '/login_friendster' do
+  url = "http://www.friendster.com/widget_login.php?api_key=#{config['friendster'][:api_key]}"
+  redirect url
+end
+
+get '/friendster_callback' do
+  url = 'http://api.friendster.com/v1/session'
+  auth_token = params['auth_token']
+  session_params = {
+    'auth_token' => params['auth_token'],
+    'api_key' => config['friendster'][:api_key]
+  }
+  normalized_params = session_params.keys.sort.map{|k| "#{k}=#{session_params[k]}" }.join
+  sig = Digest::MD5.hexdigest(normalized_params + config['friendster'][:secret])
+  session_params['sig'] = sig
+  response = Net::HTTP.start('api.friendster.com', 80){|http|
+    http.post('/v1/session?' + session_params.map{|k,v| "#{k}=#{v}" }.join("&"), '')
+  }
+  doc = Nokogiri::XML(response.body)
+  user_id = (doc.root > 'uid').text
+  session_key = (doc.root > 'session_key').text
+  
+  
+  "<dl><dt>Session Key:<dt><dd>#{session_key}</dd><dt>User ID:</dt><dd>#{user_id}</dd></dl>"
+  
 end
