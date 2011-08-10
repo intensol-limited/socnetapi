8bf051f07539ae87acee9e6d451e5fa8b8bd4476
Max Golovnia <mgolovnia@gmail.com>
2011-08-10 13:18:36 +0300

    swf post type

--- lib/apis/facebook_api.rb
+++ lib/apis/facebook_api.rb
@@ -69,7 +69,7 @@ module Socnetapi
         attachments: {
 					thumbnails: entry["picture"] ? [entry["picture"]] : [],
           images: entry["type"] == "photo" ? [@facebook.get_picture(entry["object_id"])] : [],
-					videos: entry["type"] == "video" ? [entry["link"]] : [],
+					videos: (entry["type"] == "video" || entry["type"] == "swf") ? [entry["link"]] : [],
 					link: {
 						name: entry["name"],
 						description: entry["description"],
