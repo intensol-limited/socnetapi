7d2dd15f792921500b7727aa8917e4d7c55be4fd
Jenua Boiko <jeyboy@bigmir.net>
2012-03-14 11:41:45 +0200

    add error type

--- lib/apis/error.rb
+++ lib/apis/error.rb
@@ -1,6 +1,7 @@
 class Socnetapi::Error
   class NotConnected < StandardError; end
   class SourceNotReady < StandardError; end
+  class Unauthorized < StandardError; end
   class BadResponse < StandardError
     attr_accessor :code, :http_code
     def initialize msg=nil, code=nil, http_code=nil
